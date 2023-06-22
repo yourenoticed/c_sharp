aaf65e41b0a2fe2a7c30a60fbe6cf962b6adfc39
yourenoticed <yourenoticed@gmail.com>
2023-06-22 20:00:13 +0700

    corrected logic for first hw task

--- hw02_max_of_two/Program.cs
+++ hw02_max_of_two/Program.cs
@@ -1,14 +1,14 @@
-﻿// Gets two inputted numbers and outputs the max of them
+﻿// Gets two inputted numbers and outputs which one is max and min
 Console.WriteLine("Enter the first number: ");
 int number1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number: ");
 int number2 = Convert.ToInt32(Console.ReadLine());
 if (number1 < number2)
 {
-    Console.WriteLine(number2);
+    Console.WriteLine(number2 + " is bigger than " + number1);
 }
 else if (number1 > number2)
 {
-    Console.WriteLine(number1);
+    Console.WriteLine(number1 + " is bigger than " + number2);
 }
 else Console.Write("The numbers are equal");
