b4bb76963720364e4e5df7022e3c7438cbeffaf0
yourenoticed <yourenoticed@gmail.com>
2023-06-22 18:56:35 +0700

    added a program that prints out nums in range [-a,a]

---
+++ task03_print_range_neg_a_to_a/Program.cs
@@ -0,0 +1,8 @@
+﻿// Prints out numbers from -a to a
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int start_num = -number; start_num <= number; start_num++)
+{
+    Console.Write(start_num + " ");
+}
