3083a152bcfbeaaacd162a323394f64a9360418b
yourenoticed <yourenoticed@gmail.com>
2023-06-22 19:15:05 +0700

    made printout_the_last_digit_prettier

--- task04_prints_out_the_last_digit/Program.cs
+++ task04_prints_out_the_last_digit/Program.cs
@@ -3,9 +3,7 @@ Console.Write("Enter a 3-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number >= 100 && number < 1000)
 {
-    Console.Write("The last digit of number ");
-    Console.Write(number);
-    Console.Write(" is ");
+    Console.Write("The last digit of number " + number + " is ");
     Console.WriteLine(number % 10);
 }
 else Console.WriteLine("A three-digit one...");
