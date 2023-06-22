a007cf1d82666de071e6df33cf5cb8c7eda8f5e0
yourenoticed <yourenoticed@gmail.com>
2023-06-22 19:12:08 +0700

    added logic for exucuting the program only when the number is 3-digit

--- task04_prints_out_the_last_digit/Program.cs
+++ task04_prints_out_the_last_digit/Program.cs
@@ -1,7 +1,11 @@
 ﻿// Gets the 3-digit number and returns its last digit
-Console.Write("Enter a number: ");
+Console.Write("Enter a 3-digit number: ");
 int number = Convert.ToInt32(Console.ReadLine());
-Console.Write("The last digit of number ");
-Console.Write(number);
-Console.Write(" is ");
-Console.WriteLine(number % 10);
+if (number >= 100 && number < 1000)
+{
+    Console.Write("The last digit of number ");
+    Console.Write(number);
+    Console.Write(" is ");
+    Console.WriteLine(number % 10);
+}
+else Console.WriteLine("A three-digit one...");
