b0292baf1bb2eb30be574079dfb1171e64fd569f
yourenoticed <yourenoticed@gmail.com>
2023-06-22 17:30:50 +0700

    added a program for outputting square of a number

--- task00_square_of_num/Program.cs
+++ task00_square_of_num/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Write a program that gets a number from a user and returns its square
+*/
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number * number);
+
