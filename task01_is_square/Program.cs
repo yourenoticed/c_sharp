fc95ec6498499d40895515c1168b2d912f54c527
yourenoticed <yourenoticed@gmail.com>
2023-06-22 17:57:31 +0700

    added a program for checking if num b is a square of num b

---
+++ task01_is_square/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Напишите программу, которая на вход принимает два числа и проверяет,
+является ли первое число квадратом второго.
+*/
+Console.Write("Enter a number you want to check for being a square: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter a number you want to check if it's a square of the first one: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number2 * number2 == number1)
+{
+    Console.Write("Number ");
+    Console.Write(number1);
+    Console.Write(" is a square of ");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.Write("Number ");
+    Console.Write(number1);
+    Console.Write(" isn't a square of ");
+    Console.WriteLine(number2);
+}
