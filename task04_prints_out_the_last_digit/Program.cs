832cb1f0fcebaae0e8f43e28aeb2c25ca93ae2a3
yourenoticed <yourenoticed@gmail.com>
2023-06-22 19:04:43 +0700

    added a program that returns last digit in number

---
+++ task04_prints_out_the_last_digit/Program.cs
@@ -0,0 +1,7 @@
+﻿// Gets the 3-digit number and returns its last digit
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("The last digit of number ");
+Console.Write(number);
+Console.Write(" is ");
+Console.WriteLine(number % 10);
