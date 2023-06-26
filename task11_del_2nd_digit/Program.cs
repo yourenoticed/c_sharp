d05cb311109f34189f3b65c04e593c1db9b57d6e
yourenoticed <yourenoticed@gmail.com>
2023-06-26 17:31:04 +0700

    finished task11

---
+++ task11_del_2nd_digit/Program.cs
@@ -0,0 +1,6 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Random number is {number}");
+Console.WriteLine($"The number without 2nd digit is {(number / 100) * 10 + (number % 10)}");
+/* int digit1 = number / 100;
+int digit3 = number % 10; */
+
