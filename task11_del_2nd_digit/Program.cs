35213fd4caef21e84fed644de54753e50db07a27
yourenoticed <yourenoticed@gmail.com>
2023-06-26 17:36:00 +0700

    made solution even smaller

--- task11_del_2nd_digit/Program.cs
+++ task11_del_2nd_digit/Program.cs
@@ -1,6 +1,5 @@
 ﻿int number = new Random().Next(100, 1000);
-Console.WriteLine($"Random number is {number}");
-Console.WriteLine($"The number without 2nd digit is {(number / 100) * 10 + (number % 10)}");
+Console.WriteLine($"The random number {number} without 2nd digit is {(number / 100) * 10 + (number % 10)}");
 /* int digit1 = number / 100;
 int digit3 = number % 10; 
 int answer = digit1 * 10 + digit3; */
