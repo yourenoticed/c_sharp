aa8f43bcc1979a5efec1a1cc734a0cd5b767ac2e
yourenoticed <yourenoticed@gmail.com>
2023-06-26 17:22:31 +0700

    finished task09

--- task09_max_digit_of_random_num_10_99/Program.cs
+++ task09_max_digit_of_random_num_10_99/Program.cs
@@ -3,10 +3,9 @@ int digit1 = number / 10;
 int digit2 = number % 10;
 if (digit1 > digit2)
 {
-    Console.WriteLine($"The max digit is {digit1}");
+    Console.WriteLine($"The max digit of number {number} is {digit1}");
 }
-else if (digit1 < digit2)
+else
 {
-    Console.WriteLine($"The max digit is {digit2}")
+    Console.WriteLine($"The max digit of number {number} is {digit2}");
 }
-else Console.WriteLine($"The digits of the number {number} are equal")
