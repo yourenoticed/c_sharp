1284fbd6ab9ee9b3f796c2a2c21273a175af74e6
yourenoticed <yourenoticed@gmail.com>
2023-06-26 18:26:59 +0700

    finished task16

---
+++ task16_is_square/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter the first number: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the second number: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1 / number2 == number2 || number2 / number1 == number1)
+{
+    Console.WriteLine("True");
+}
+else Console.WriteLine("False");
