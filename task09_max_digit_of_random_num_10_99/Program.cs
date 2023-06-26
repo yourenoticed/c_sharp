db8b32742b9f8282fc28cb54a5732e96a61b4305
yourenoticed <yourenoticed@gmail.com>
2023-06-26 17:13:01 +0700

    did task 09 find max digit of 2-digit num

---
+++ task09_max_digit_of_random_num_10_99/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Next(10, 100);
+int digit1 = number / 10;
+int digit2 = number % 10;
+if (digit1 > digit2)
+{
+    Console.WriteLine($"The max digit is {digit1}");
+}
+else if (digit1 < digit2)
+{
+    Console.WriteLine($"The max digit is {digit2}")
+}
+else Console.WriteLine($"The digits of the number {number} are equal")
