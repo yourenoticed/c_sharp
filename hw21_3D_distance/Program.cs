fd331e4dbe9de744d442200ce2d7f4dc7414e0a7
yourenoticed <yourenoticed@gmail.com>
2023-06-29 19:39:16 +0700

    did homework tasks for 29.06

---
+++ hw21_3D_distance/Program.cs
@@ -0,0 +1,18 @@
+﻿double ThreeDimensionDistance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2) + Math.Pow(z1 - z2, 2));  
+}
+
+Console.Write("Enter first x coordinate: ");
+double xFirst = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter first y coordinate: ");
+double yFirst = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter first z coordinate: ");
+double zFirst = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter second x coordinate: ");
+double xSecond = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter second y coordinate: ");
+double ySecond = Convert.ToDouble(Console.ReadLine());
+Console.Write("Enter second z coordinate: ");
+double zSecond = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine(ThreeDimensionDistance(xFirst, yFirst, zFirst, xSecond, ySecond, zSecond));
---
+++ hw23_cubes_1_to_n/Program.cs
@@ -0,0 +1,23 @@
+﻿string Cubes(int n)
+{
+    string cubes = "1";
+    if (n > 2)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            cubes = $"{cubes} {i * i * i}";
+        }
+    }
+    else
+    {
+        for (int i = 0; i >= n; i--)
+        {
+            cubes = $"{cubes} {i * i * i}";
+        } 
+    }
+    return cubes;
+}
+
+Console.Write("Enter a number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Cubes(number));
