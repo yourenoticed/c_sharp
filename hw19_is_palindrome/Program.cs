c8b5cadef06688e7b63940cf48a2b0e47695fd18
yourenoticed <yourenoticed@gmail.com>
2023-06-30 13:18:02 +0700

    condition number > 0 was unnecessary

--- hw19_is_palindrome/Program.cs
+++ hw19_is_palindrome/Program.cs
@@ -4,17 +4,14 @@
     int temp = number;
     int reversed = 0;
 // if (number >= 10000 && number < 100000)  // add this condition for getting the solution for 5-digit numbers
-    if (number > 0) // в случае, если число будет отрицательным, то будет читаться как 1234-, т.е. не может быть палиндромом по определению
+    
+    while (number > 0)
     {
-        while (number > 0)
-        {
-            reversed = reversed * 10 + number % 10;
-            number /= 10;
-        }
-        if (temp == reversed) return true;
-        else return false;
+        reversed = reversed * 10 + number % 10;
+        number /= 10;
     }
+    if (temp == reversed) return true;
     else return false;
 }
 
-Console.WriteLine(IsPalindrome(13331));
+Console.WriteLine(IsPalindrome(-13331));
